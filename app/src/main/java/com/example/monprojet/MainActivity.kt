2c08831dbb868157ccef1d8cd165cf656fdabd89
https://github.com/Semeydianenirvole-cloud <Semeydianenirvole@icloud.com>
2025-11-28 03:01:29 -0800

    yon app avek yon kesyon

--- app/src/main/java/com/example/monprojet/MainActivity.kt
+++ app/src/main/java/com/example/monprojet/MainActivity.kt
@@ -1,25 +1,53 @@
 package com.example.monprojet
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val data: Intent? = result.data
+        if (data != null) { // Check that we have data returned
+            val question = intent.getStringExtra("question_clef")
+            val answer = intent.getStringExtra("reponse_clef")
+
+            //log the value of the stings for easier debugging
+            Log.i("MainActivity", "string1: $question")
+            Log.i("MainActivity", "string2: $answer")
+            findViewById<TextView>(R.id.milie).text = question
+            findViewById<TextView>(R.id.milie2).text = answer
+        } else {
+            Log.i("MainActivity","Returned null data from MainActivity2")
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val questionText = findViewById<TextView>(R.id.milie)
+        val answertext = findViewById<TextView>(R.id.milie2)
+        val icon = findViewById<ImageView>(R.id.imageView)
 
-        findViewById<View>(R.id.milie).setOnClickListener {
-            findViewById<View>(R.id.milie).visibility = View.INVISIBLE
-            findViewById<View>(R.id.milie2).visibility = View.VISIBLE
+           icon.setOnClickListener {
+               val intent = Intent(this, MainActivity2::class.java)
+               resultLauncher.launch(intent)
+           }
+        questionText.setOnClickListener {
+            questionText.visibility = View.INVISIBLE
+            answertext.visibility = View.VISIBLE
         }
-        findViewById<View>(R.id.milie2).setOnClickListener {
-            findViewById<View>(R.id.milie).visibility = View.VISIBLE
-            findViewById<View>(R.id.milie2).visibility = View.INVISIBLE
+        answertext.setOnClickListener {
+            questionText.visibility = View.VISIBLE
+            answertext.visibility = View.INVISIBLE
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
--- app/src/main/java/com/example/monprojet/MainActivity2.kt
+++ app/src/main/java/com/example/monprojet/MainActivity2.kt
@@ -1,6 +1,9 @@
 package com.example.monprojet
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,40 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+
+        val questionText = findViewById<EditText>(R.id.hae_question)
+        val answertext = findViewById<EditText>(R.id.header_title)
+        val save_button = findViewById<ImageView>(R.id.imageView4)
+        val cancel_button = findViewById<ImageView>(R.id.imageView5)
+
+        cancel_button.setOnClickListener {
+             setResult(RESULT_CANCELED)
+             finish()
+        }
+
+        save_button.setOnClickListener {
+            val userQuestion = questionText.text.toString()
+            val userAnswer = answertext.text.toString()
+
+            val data = Intent() //create a new Intent, this is where we will put our data
+            data.putExtra(
+                "question_clef",
+                userQuestion
+            )
+             data.putExtra(
+                 "reponse_clef",
+                 userAnswer
+             ) // puts another string into the Intent, with the key as 'string2
+
+            setResult(RESULT_OK, data) // set result code and bundle data for response
+
+            finish() // closes this activity and pass data to the original activity that launched this activity
+        }
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
